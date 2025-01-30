34bbe4a015895e502a5b24d50d2ea302be9913eb
lyhor <phanlyhor369@gmail.com>
2025-01-30 21:01:49 +0700

    update

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var connectionDB = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "auth"
+  host: "bs3p4xguks7fdxaznsia-mysql.services.clever-cloud.com",
+  user: "umedxafaihuq72ju",
+  password: "umedxafaihuq72ju",
+  database: "bs3p4xguks7fdxaznsia"
 });
 
 connectionDB.connect(function(err) {
